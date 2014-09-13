909c4c4d15d9a35926c05738403652fa4e22dfb7
Javier Choque <jchoque@fi.uba.ar>
2014-09-13 16:15:59 -0300

    adaptacion del modelo en backend de la publicacion

--- android-mileem/app/src/main/java/ar/uba/fi/proyectos2/mileem/model/Publication.java
+++ android-mileem/app/src/main/java/ar/uba/fi/proyectos2/mileem/model/Publication.java
@@ -1,25 +1,92 @@
 package ar.uba.fi.proyectos2.mileem.model;
 
+import java.util.Date;
+
 /**
  * Created by javier on 07/09/14.
  */
 public class Publication {
-    private String name;
-    private String street;
+
+    private int id;
+    private String effective_date;
+    private String operation;
+    private String address;
+    private int floor;
+    private String apartment;
+    private int number_spaces;
+    private int surface;
     private int price;
+    private int expenses;
+    private int antiquity;
+    private String description;
+    private String additional_info;
+    private String url;
 
-    public Publication(String name, String street, int price) {
-        this.name = name;
-        this.street = street;
-        this.price = price;
+    public Publication() {
     }
 
-    public String getName() {
-        return name;
+    public int getId() {
+        return id;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEffective_date() {
+        return effective_date;
+    }
+
+    public void setEffective_date(String effective_date) {
+        this.effective_date = effective_date;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public void setOperation(String operation) {
+        this.operation = operation;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public String getApartment() {
+        return apartment;
+    }
+
+    public void setApartment(String apartment) {
+        this.apartment = apartment;
+    }
+
+    public int getNumber_spaces() {
+        return number_spaces;
+    }
+
+    public void setNumber_spaces(int number_spaces) {
+        this.number_spaces = number_spaces;
+    }
+
+    public int getSurface() {
+        return surface;
+    }
+
+    public void setSurface(int surface) {
+        this.surface = surface;
     }
 
     public int getPrice() {
@@ -30,11 +97,43 @@ public class Publication {
         this.price = price;
     }
 
-    public String getStreet() {
-        return street;
+    public int getExpenses() {
+        return expenses;
     }
 
-    public void setStreet(String street) {
-        this.street = street;
+    public void setExpenses(int expenses) {
+        this.expenses = expenses;
+    }
+
+    public int getAntiquity() {
+        return antiquity;
+    }
+
+    public void setAntiquity(int antiquity) {
+        this.antiquity = antiquity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAdditional_info() {
+        return additional_info;
+    }
+
+    public void setAdditional_info(String additional_info) {
+        this.additional_info = additional_info;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
