9a73504cfa4577d8ac2ee78a82c1fb473f527b17
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-09-16 23:27:45 -0300

    Traduzco a español los mensajes de la publicacion, y titulos de las pantallas.

--- web-mileem/app/controllers/publications_controller.rb
+++ web-mileem/app/controllers/publications_controller.rb
@@ -40,7 +40,7 @@ class PublicationsController < ApplicationController
 
     respond_to do |format|
       if @publication.save
-        format.html { redirect_to @publication, notice: 'Publication was successfully created.' }
+        format.html { redirect_to @publication, notice: 'La publicación fue creada exitosamente.' }
         format.json { render :show, status: :created, location: @publication }
       else
         format.html { render :new }
@@ -54,7 +54,7 @@ class PublicationsController < ApplicationController
   def update
     respond_to do |format|
       if @publication.update(publication_params)
-        format.html { redirect_to @publication, notice: 'Publication was successfully updated.' }
+        format.html { redirect_to @publication, notice: 'La publicación fue actualizada exitosamente.' }
         format.json { render :show, status: :ok, location: @publication }
       else
         format.html { render :edit }
@@ -68,7 +68,7 @@ class PublicationsController < ApplicationController
   def destroy
     @publication.destroy
     respond_to do |format|
-      format.html { redirect_to publications_url, notice: 'Publication was successfully destroyed.' }
+      format.html { redirect_to publications_url, notice: 'La publicación fue destruida exitosamente.' }
       format.json { head :no_content }
     end
   end
