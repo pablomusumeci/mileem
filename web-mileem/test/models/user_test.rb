c69a0fa66235fe9fe5f9dd184fc817063b99ac8a
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-09-19 17:27:00 -0300

    Agrego tests para la creacion del user.

--- web-mileem/test/models/user_test.rb
+++ web-mileem/test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@user = User.new
+		@user.email = 'test@user.com'
+		@user.password = 'password'
+	end
+
+	def test_create_user_ok
+		assert @user.save, 'Validate all fields have values according to its validations'
+	end
+
+	def test_email_required
+		@user.email = ''
+		assert_not @user.save, 'Validate email not blank'
+	end
+
+	def test_password_required
+		@user.password = ''
+		assert_not @user.save, 'Validate password not blank'
+	end
+
+	def test_password_length
+		@user.password = '12345'
+		assert_not @user.save, 'Validate password 6 char long or more'
+	end
 end
