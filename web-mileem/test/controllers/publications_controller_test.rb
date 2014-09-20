ee5ea7c42908513272169997f4f0972cdbf806d9
lrodriguez <leandro.rodriguez@prosoftwarefactory.com.ar>
2014-09-20 18:13:24 -0300

    Corren todos los tests. Incluso descomente los que habia comentado. A nadie le importa, a mi si. KB

--- web-mileem/test/controllers/publications_controller_test.rb
+++ web-mileem/test/controllers/publications_controller_test.rb
@@ -5,13 +5,9 @@ class PublicationsControllerTest < ActionController::TestCase
   setup do
     @publication = publications(:one)
     @publication.effective_date = Date.today
-    #@barrio = Neighbourhood.create(:name => "Belgrano")
-    #@publication.neighbourhood = @barrio
     
-    @user = User.create(:email => "usarioTest@prueba.com", :password => 'password')
+    @user = users(:one)
     sign_in @user
-    @publication.user_id = @user.id
-    
   end
 
   test "should get index" do
@@ -20,18 +16,18 @@ class PublicationsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:publications)
   end
 
-  #test "should get new" do
-  #  get :new
-  #  assert_response :success
-  #end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-  #test "should create publication" do
-  #  assert_difference('Publication.count') do
-  #    post :create, publication: { additional_info: @publication.additional_info, address: @publication.address, antiquity: @publication.antiquity, apartment: @publication.apartment, description: @publication.description, effective_date: Date.today, expenses: @publication.expenses, floor: @publication.floor, number_spaces: @publication.number_spaces, operation: @publication.operation, price: @publication.price, surface: @publication.surface, neighbourhood: @publication.neighbourhood }
-  #  end
-  #
-  #  assert_redirected_to publication_path(assigns(:publication))
-  #end
+  test "should create publication" do
+    assert_difference('Publication.count') do
+      post :create, publication: { additional_info: @publication.additional_info, address: @publication.address, antiquity: @publication.antiquity, apartment: @publication.apartment, description: @publication.description, effective_date: @publication.effective_date, expenses: @publication.expenses, floor: @publication.floor, number_spaces: @publication.number_spaces, operation: @publication.operation, price: @publication.price, surface: @publication.surface, currency_id: 2, property_type_id: 1, neighbourhood_id: 1, user_id: 1 }
+    end
+  
+    assert_redirected_to publication_path(assigns(:publication))
+  end
 
   test "should show publication" do
     get :show, id: @publication
@@ -43,10 +39,10 @@ class PublicationsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  #test "should update publication" do
-  #  patch :update, id: @publication, publication: { additional_info: @publication.additional_info, address: @publication.address, antiquity: @publication.antiquity, apartment: @publication.apartment, description: @publication.description, effective_date: @publication.effective_date, expenses: @publication.expenses, floor: @publication.floor, number_spaces: @publication.number_spaces, operation: @publication.operation, price: @publication.price, surface: @publication.surface }
-  #  assert_redirected_to publication_path(assigns(:publication))
-  #end
+  test "should update publication" do
+    patch :update, id: @publication, publication: { additional_info: @publication.additional_info, address: @publication.address, antiquity: @publication.antiquity, apartment: @publication.apartment, description: @publication.description, effective_date: @publication.effective_date, expenses: @publication.expenses, floor: @publication.floor, number_spaces: @publication.number_spaces, operation: @publication.operation, price: @publication.price, surface: @publication.surface }
+    assert_redirected_to publication_path(assigns(:publication))
+  end
 
   test "should destroy publication" do
     assert_difference('Publication.count', -1) do
