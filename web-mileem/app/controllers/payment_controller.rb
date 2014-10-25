b0a5c6866e3efb4ffa52d557095e01741a067a23
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-10-25 15:58:26 -0300

    #US14 Error en el payment controller, no levantaba la app por un typo.

--- web-mileem/app/controllers/payment_controller.rb
+++ web-mileem/app/controllers/payment_controller.rb
@@ -1,8 +1,9 @@
-}class PaymentController < ApplicationController
+class PaymentController < ApplicationController
+  # GET /payment/notification
   def notification
   	@topic = params[:topic]
   	@payment_id = params[:id]
-  	Rails.logger.info "NOTIFICACION RECIBIDA ID : #{@payment_ID}"
+  	Rails.logger.info "NOTIFICACION RECIBIDA ID : #{@payment_id}"
   	@publication_id = @payment_id.split("-")[2].to_i
   	@publication = Publication.find(@publication_id)
   	p "Antes de modificarla #{@publication}"
