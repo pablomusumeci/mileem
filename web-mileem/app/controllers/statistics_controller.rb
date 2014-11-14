52bc8b816ea544e98a0295029b91421dadaa99f1
pablomusumeci <pablomusumeci@yahoo.com.ar>
2014-11-14 15:58:03 -0300

    [US#24] Agrego parametro para elegir la moneda en la que se devuelven los promedios

--- web-mileem/app/controllers/statistics_controller.rb
+++ web-mileem/app/controllers/statistics_controller.rb
@@ -1,4 +1,5 @@
 class StatisticsController < ApplicationController
+  $conversiones = {"USD" => {"USD" => 1, "$" => 10}, "$" => {"USD" => 0.1, "$" => 1}}
   def spaces_distribution
   	publications = Publication.select("number_spaces, COUNT(1) as quantity").group(:number_spaces)
   	result = []
@@ -12,6 +13,9 @@ class StatisticsController < ApplicationController
 
   def near_prices
   	barrio_parametro = Neighbourhood.find(params[:neighbourhood_id])
+  	moneda_deseada = params[:currency]
+  	moneda_deseada = "$" if moneda_deseada.nil?
+
   	ids_vecinos = NearNeighbourhood.where(neighbourhood_id_1: barrio_parametro.id).pluck(:neighbourhood_id_2)
 
   	# Quiero tambien el promedio del barrio pasado como parametro
@@ -19,7 +23,6 @@ class StatisticsController < ApplicationController
 
   	# Traigo publicaciones activas de los barrios vecinos, del tipo pasado por parametro
   	query = Publication.where(neighbourhood_id: ids_vecinos).where(operation: params[:operation]).select{|p| (p.isAvailable) and (p.payment_status == "Realizado")}
-  	conversiones = {"USD" => {"USD" => 1, "$" => 10}, "$" => {"USD" => 0.1, "$" => 1}}
   	resultados_parciales = {}
 
   	#Para cada barrio vecino
@@ -30,11 +33,11 @@ class StatisticsController < ApplicationController
   			total = 0
 	  		publications_from_this_neighbourhood.each do |p|
 	  			# Convierto moneda a pesos
-	  			factor = conversiones[p.currency.abreviatura]["$"]
+	  			factor = $conversiones[p.currency.abreviatura][moneda_deseada]
 	  			# Sumo el precio en pesos de esta publicacion
 	  			total += (p.price * factor) / p.surface if (p.surface > 0)
 	  		end
-	  		resultados_parciales[barrio] = total.to_f / publications_from_this_neighbourhood.size
+	  		resultados_parciales[barrio] = (total.to_f / publications_from_this_neighbourhood.size).round(2) # 2 decimales
   		end
   	end
 
@@ -42,7 +45,7 @@ class StatisticsController < ApplicationController
 
   	# Los barrios vecinos que no tienen publicaciones no se incluyen
   	resultados_parciales.each do |k,v|
-  		resultado << {"neighbourhood_name" => Neighbourhood.find(k).name, "average" => v}
+  		resultado << {"neighbourhood_name" => Neighbourhood.find(k).name, "average" => v, "currency" => moneda_deseada}
   	end
 
 	respond_to do |format|
